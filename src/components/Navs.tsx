c649915551f8522261766db0b636fbadf389b873
abdizamedmo <abdizamedmo@gmail.com>
2022-12-14 14:31:53 +0300

    Drawer is ending now, nav items is listed and icons

---
+++ src/components/Navs.tsx
@@ -0,0 +1,46 @@
+import { MdChairAlt, MdPerson } from 'react-icons/md';
+
+export const navConfig = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: (
+      <MdChairAlt className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+    )
+  },
+  {
+    name: 'Users',
+    url: '/users',
+    icon: (
+      <MdPerson className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+    )
+  },
+  {
+    name: 'Users',
+    url: '/users',
+    icon: (
+      <MdPerson className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+    )
+  },
+  {
+    name: 'Users',
+    url: '/users',
+    icon: (
+      <MdPerson className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+    )
+  },
+  {
+    name: 'Users',
+    url: '/users',
+    icon: (
+      <MdPerson className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+    )
+  },
+  {
+    name: 'Users',
+    url: '/users',
+    icon: (
+      <MdPerson className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+    )
+  }
+];
